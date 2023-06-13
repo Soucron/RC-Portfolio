d194e96f527b2376946b153d485999d1838c7c3f
Soucron <85357498+Soucron@users.noreply.github.com>
2023-06-13 08:50:15 +0200

    Update Footer.tsx

--- src/footer/Footer.tsx
+++ src/footer/Footer.tsx
@@ -2,27 +2,38 @@ import React from 'react';
 import style from './Footer.module.css'
 import Network from './network/Network';
 
+type NetworkType = {
+    id: str,
+    href: string,
+    src: string
+}
+    
 
-
-const Footer = () => {
+export  const Footer = () => {
+    
+    const Networks: NetworkType [] = {
+        [id: telegram, href: , src:],
+        [id: linkedIn, href: , src:],
+        [id: gitHub, href: , src: ]
+    }
+    
+    
     return (
         <div className={style.footerBlock}>
             <div className={style.footerContainer}>
                 <h2>Navitski Pavel</h2>
                <div className={style.networks}>
-                <Network/>
-                <Network/>
-                <Network/>
-                <Network/>
-                <Network/>
-                <Network/>
-                <Network/>
-                <Network/>
+                   {Networks.map(n => {
+                       return ( 
+                           <Network 
+                               id={n.id}
+                               href={n.href}
+                               src={n.src}/>
+                   }
+              
                </div>
                 <h2> 2023 </h2>
             </div>
         </div>
     );
 };
-
-export default Footer;
